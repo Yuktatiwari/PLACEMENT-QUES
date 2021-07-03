be36551494eb9189c47fc13fa7f33cf886d60ca5
Yuktatiwari <59305259+Yuktatiwari@users.noreply.github.com>
2021-07-03 18:46:16 +0530

    Update 2.cpp

--- DAY 2/2.cpp
+++ DAY 2/2.cpp
@@ -15,38 +15,6 @@ void findIntersection(int arr1[], int arr2[], int m, int n)
         }
     }
 }
-void findUnion(int arr1[], int arr2[], int m, int n)
-{
-    vector<int> v1;
-    v1.push_back(arr1[0]);
-    for (int i = 1; i < m; i++)
-    {
-        for (int k = 0; k < v1.size(); k++)
-        {
-            if (arr1[i] == v1[k])
-            {
-                break;
-            }
-        }
-        v1.push_back(arr1[i]);
-    }
-    for (int x = 0; x < n; x++)
-    {
-        for (int y = 0; y < v1.size(); y++)
-        {
-            if (arr2[x] == v1[y])
-            {
-                break;
-            }
-        }
-        v1.push_back(arr2[x]);
-    }
-    cout << "Union of two arrays: ";
-    for (int z = 0; z < v1.size(); z++)
-    {
-        cout << v1[z] << " ";
-    }
-}
 int main()
 {
     int m, n;
@@ -64,6 +32,5 @@ int main()
         cin >> arr2[i];
     }
     findIntersection(arr1, arr2, m, n);
-    findUnion(arr1, arr2, m, n);
     return 0;
 }
